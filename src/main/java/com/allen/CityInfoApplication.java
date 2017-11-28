eecb8007b8250f54b9ff2feefc1e6aa460bb56da
AllenWish <1697501868@qq.com>
2017-11-28 22:22:01 +0800

    添加访问路径注释

--- src/main/java/com/allen/CityInfoApplication.java
+++ src/main/java/com/allen/CityInfoApplication.java
@@ -3,6 +3,11 @@ package com.allen;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * 访问地址 localhost:server.port/server.context-path
+ * 默认为 localhost:8080/
+ * 这里我配置了 yml  port为8033 添加了 context-path  为cityInfo
+ */
 @SpringBootApplication
 public class CityInfoApplication {
 
